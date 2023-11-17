7954c4af78b1d1376e00f6e32e53fd3acc82f489
RUFUS-17-4 <rufus170404@gmail.com>
2023-11-17 11:41:44 +0530

    save

---
+++ classwork/src/hello.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function hello(){
+    <h1>HELLO WORLD!</h1>
+}
+export default hello;
--- classwork/src/index.js
+++ classwork/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-
+import hello from "./hello";
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
-  <App />
+  <hello />
 );
